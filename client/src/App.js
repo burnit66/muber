cbc1d22f4e6aa43e701d27d7f9817943ba2aa495
joe <burnett.joe@gmail.com>
2019-01-08 13:24:39 -0500

    cleaning up

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './CSS/App.css'
-import LoginContainer from './Register'
+// import LoginContainer from './Register'
+import Splash from './Splash'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginContainer/>
+        <Splash/>
+        {/* <LoginContainer/> */}
       </div>
     );
   }
